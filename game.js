4cb00ad9744d9aa6e02b3d3243e6fc59bafc93a1
Viogoo <158023320+Viogoo@users.noreply.github.com>
2024-04-16 15:50:34 +0300

    tyery

--- game.js
+++ game.js
@@ -1,11 +1,12 @@
-// Ініціалізація Phaser
+// Створюємо сцену
 var config = {
     type: Phaser.AUTO,
-    width: 1000,
-    height: 500,
+    width: 1920,
+    height: 1080,
     physics: {
         default: 'arcade',
         arcade: {
+            gravity: { y: 170 },
             debug: false,
         }
     },
@@ -13,167 +14,267 @@ var config = {
         preload: preload,
         create: create,
         update: update
-    },
+    }
 };
 
+// Змінні
 var game = new Phaser.Game(config);
-var spaceship;
-var collectTrash;
-var gameOver;
-var updateTimer;
-var trash;
-var inputx;
-var inputy;
+var cursors;
+var platforms;
+var player;
+var bombs;
+var stars;
+var score = 0;
+var scoreText;
+var life = 5;
+var lifeText;
+var worldWidth = 9600;
+var enemy;
+var enemyLives = 5;
+var enemyText;
 
-
-
-
-
-// Попереднє завантаження ресурсів
 function preload() {
-    this.load.image('space', 'assets/space.jpg');
-    this.load.image('spaceship', 'assets/spaceship.png');
-    this.load.image('trash', 'assets/tresh.png');
-    this.load.image('lasers', 'assets/lasers.png');
+    // Додаємо клавіші керування
+    cursors = this.input.keyboard.createCursorKeys();
+    // Завантажуємо спрайти
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('platform1', 'assets/10.png');
+    this.load.image('platform2', 'assets/11.png');
+    this.load.image('platform3', 'assets/12.png');
+    this.load.image('ground', 'assets/1.png');
+    this.load.image('bullet', 'assets/bullet.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('tree', 'assets/3.png');
+    this.load.image('ts', 'assets/5.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('enemy', 'assets/enemy.png');
+    this.load.spritesheet('dude', 'assets/2.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-
-
-// Основна функція гри
+// Додаємо спрайти до сцени
 function create() {
-   
-   
-   
-   
-    // Додавання фону
-    this.add.image(0, 0, 'space').setOrigin(0);
+    this.add.tileSprite(0, 0, worldWidth, 1080, "sky")
+        .setOrigin(0, 0)
+        .setScale(1)
+        .setDepth(0);
 
-    
-    
-    // Додавання космічного корабля
-    spaceship = this.physics.add.sprite(50, 250, 'spaceship').setScale(0.2).setCollideWorldBounds(true);
+    // Додаємо генерацію платформ
+    platforms = this.physics.add.staticGroup();
 
-
-
-//додавання космічного сміття
-    trash = this.physics.add.group({
-        key: 'trash',
-        repeat: 11,
-        setXY: { x: 900, y: 100, stepX: 70 } 
-    });
-
-
-
-       // Додавання лазерів
-       lasers = this.physics.add.group( {key: ' lasers'});
-    
-
-
-     // Генерування сміття з різних сторін
-     generateTrash();
-    
-    
-    
-   
-   
-   
-    // Збір сміття
-    this.physics.add.collider(spaceship, trash, collectTrash, null, this);
-
-   
-   
-    // Перевірка колізії космічного корабля з лазерами
-    this.physics.add.overlap(spaceship, lasers, gameOver, null, this);
-   
-    // Рахунок зібраного сміття
-    this.score = 0;
-    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
-
-
-
-
-
-   
-    // Таймер або час гри
-    this.timeText = this.add.text(800, 16, 'Time: 60', { fontSize: '32px', fill: '#fff' });
-    this.endTime = this.time.addEvent({ delay: 60000, callback: gameOver, callbackScope: this });
-    this.timer = this.time.addEvent({ delay: 1000, callback: updateTimer, callbackScope: this, repeat: 59 });
- 
-
-     // Генерування лазерів
-     this.time.addEvent({ delay: 1000, callback: generateLasers, callbackScope: this, loop: true });
+    for (var x = 0; x < worldWidth; x = x + 128) {
+        platforms
+            .create(x, 1050, "ground")
+            .setOrigin(0, 0)
+            .refreshBody(1);
     }
 
+    // Летючі платформи
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(3000, 800)) {
+        platforms.create(x, 850, 'platform1');
 
-// Генерування сміття з різних сторін
-function generateTrash() {
-    for (let i = 0; i < 12; i++) {
-        let x = Phaser.Math.Between(0, game.config.width);
-        let y = Phaser.Math.Between(0, game.config.height);
-        let trashPiece = trash.create(x, y, 'trash');
-        trashPiece.setVelocityX(Phaser.Math.Between(-200, 200)); // Додано хаотичний рух
-    }
-}
-
-// Генерування лазерів
-function generateLasers() {
-    let x = Phaser.Math.Between(0, game.config.width);
-    let y = Phaser.Math.Between(0, game.config.height);
-    let laser = lasers.create(x, y, 'lasers');
-    laser.setVelocity(Phaser.Math.Between(-300, 300), Phaser.Math.Between(-300, 300)); // Додано хаотичний рух
-}
-
-
-// Функція оновлення
-function update() {
-
-   
-    this.physics.moveTo(spaceship, game.input.mousePointer.x, game.input.mousePointer.y, 400);
-    // Рух космічного корабля
-    if (this.input.activePointer.isDown) {}
-   
-    
-    
-    // Видалення сміття, яке виходить за межі екрану
-    trash.children.iterate(function(child) {
-        if (child.x < -50) {
-            child.x = 1100;
+        var i;
+        for (i = 1; i < Phaser.Math.Between(0, 5); i++) {
+            platforms.create(x + 128 * i, 850, 'platform2');
         }
+
+        platforms.create(x + 128 * i, 850, 'platform3');
+    }
+
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(1000, 800)) {
+        platforms.create(x, 650, 'platform1');
+
+        var i;
+        for (i = 1; i < Phaser.Math.Between(0, 5); i++) {
+            platforms.create(x + 128 * i, 650, 'platform2');
+        }
+
+        platforms.create(x + 128 * i, 650, 'platform3');
+    }
+
+    // Додаємо кущі
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(1700, 100)) {
+        var ts = this.physics.add.staticGroup();
+        ts.create(x, 1080 - 25, 'ts')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(1, 1.5))
+            .setDepth(Phaser.Math.Between(9, 10));
+    }
+
+    // Додаємо дерева
+    var tree = this.physics.add.staticGroup();
+    for (var x = 0; x < worldWidth; x = x + Phaser.Math.FloatBetween(2000, 500)) {
+        tree.create(x, 1080 - 25, 'tree')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(1.5, 1.7))
+            .setDepth(Phaser.Math.Between(1, 2));
+    }
+
+    // Гравець
+    player = this.physics.add.sprite(100, 700, 'dude');
+    player.setBounce(0.2)
+        .setDepth(Phaser.Math.Between(4, 5))
+        .setCollideWorldBounds(true);
+    player.body.setGravityY(300);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
     });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
+
+    bombs = this.physics.add.group();
+    this.physics.add.collider(bombs, platforms);
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+
+    // Камера
+    this.cameras.main.setBounds(0, 0, worldWidth, 1080);
+    this.physics.world.setBounds(0, 0, worldWidth, 1080);
+    this.cameras.main.startFollow(player);
+
+    // Текст "Enemy Lives"
+    enemyText = this.add.text(50, 150, 'Enemy Lives: ' + enemyLives, { fontSize: '40px', fill: '#FFF' })
+        .setOrigin(0, 0)
+        .setScrollFactor(0);
+
+    // Додаємо очки рахунку
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 50,
+        setXY: { x: 12, y: 0, stepX: 500 }
+    });
+
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
+
+    // Додаємо рахунок і життя
+    scoreText = this.add.text(100, 100, 'Score: 0', { fontSize: '40px', fill: '#FFF' })
+        .setOrigin(0, 0)
+        .setScrollFactor(0);
+
+    lifeText = this.add.text(1500, 100, showLife(), { fontSize: '40px', fill: '#FFF' })
+        .setOrigin(0, 0)
+        .setScrollFactor(0);
+
+    // Кнопка рестарту
+    var resetButton = this.add.text(938, 70, '♻️', { fontSize: '60px', fill: '#FFF' })
+        .setInteractive()
+        .setScrollFactor(0);
+
+    resetButton.on('pointerdown', function () {
+        console.log('restart');
+        location.reload();
+    });
+
+    // Додаємо ворога
+    enemy = this.physics.add.sprite(1000, 1080 - 150, 'enemy');
+    this.physics.add.collider(enemy, platforms);
+    this.physics.add.collider(player, enemy, hitEnemy, null, this);
+
+    // Додаємо слухача подій для клавіші "Space"
+    this.input.keyboard.on('keydown-SPACE', shootBullet);
+
+    // Ініціалізуємо групу куль
+    bullets = this.physics.add.group();
 }
 
+function update() {
+    // Додаємо керування гравцем
+    if (cursors.left.isDown) {
+        player.setVelocityX(-500);
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(500);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
 
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-520);
+    }
 
-
-// Функція збору сміття
-function collectTrash(spaceship, trash) {
-    trash.disableBody(true, true);
-    this.score += 10;
-    this.scoreText.setText('Score: ' + this.score);
+    // Перевіряємо зіткнення гравця з ворогом
+    if (Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), enemy.getBounds())) {
+        player.setVelocityX(0);
+    }
 }
 
-
-
-
-
-// // Функція оновлення таймера
-// function updateTimer() {
-//     this.timeText.setText('Time: ' + (this.timer.repeatCount - this.timer.getOverallProgress()));
+// function hitEnemy(player, enemy) {
+//     enemyLives--;
+//     let enemyLifeIcons = '';
+//     for (let i = 0; i < enemyLives; i++) {
+//         enemyLifeIcons += '👾';
+//     }
+//     enemyText.setText('Enemy Lives: ' + enemyLifeIcons);
+    
+//     player.setVelocityX(Phaser.Math.FloatBetween(-200, 200));
+//     player.setVelocityY(-100);
 // }
 
-
- // Функція оновлення таймера
- function updateTimer() {
-    this.timeText.setText('Time: ' + this.timer.repeatCount);
+function hitEnemy(player, enemy) {
+    enemyLives--;
+    let enemyLifeIcons = '';
+    for (let i = 0; i < enemyLives; i++) {
+        enemyLifeIcons += '👾';
+    }
+    enemyText.setText('Enemy Lives: ' + enemyLifeIcons);
+    
+    // Відкидаємо гравця вгору та трохи в протилежну сторону
+    if (player.x < enemy.x) {
+        player.setVelocityX(200);
+    } else {
+        player.setVelocityX(-200);
+    }
+    player.setVelocityY(-400);
+}
+function collectStar(player, star) {
+    star.disableBody(true, true);
+    score += 10;
+    scoreText.setText('Score: ' + score);
 }
 
+function showLife() {
+    var lifeLine = 'Life: ';
+    for (var i = 0; i < life; i++) {
+        lifeLine += '❤️';
+    }
+    return lifeLine;
+}
 
-
-
-
-// Функція завершення гри
-function gameOver() {
-    this.add.text(400, 250, 'GAME OVER', { fontSize: '64px', fill: '#fff' });
+function hitBomb(player, bomb) {
     this.physics.pause();
-   
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+}
+
+function shootBullet() {
+    var bullet = bullets.create(player.x, player.y, 'bullet');
+    if (player.body.velocity.x < 0) {
+        bullet.setVelocityX(-2000);
+    } else {
+        bullet.setVelocityX(2000);
+    }
 }
